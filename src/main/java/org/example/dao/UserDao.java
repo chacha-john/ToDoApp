5dc6793eec6841d9572fd877ad0ed0ebc53cab0a
chacha <chachaerickjo@gmail.com>
2022-05-23 22:17:51 +0300

    ft - implement methods provided by the interface

---
+++ src/main/java/org/example/dao/UserDao.java
@@ -0,0 +1,55 @@
+package org.example.dao;
+
+import org.example.interfaces.IUser;
+import org.example.models.User;
+import org.sql2o.Connection;
+import org.sql2o.Sql2oException;
+
+public class UserDao implements IUser {
+
+    @Override
+    public void createAccount(Connection connection, User user) {
+        try{
+            String query = "INSERT INTO users (name,email,phone,password) VALUES(:name,:email,:phone,:password";
+             int id = (int) connection.createQuery(query)
+                    .bind(user)
+                    .executeUpdate()
+                    .getKey();
+             user.setId(id);
+        } catch (Sql2oException ex){
+            throw new RuntimeException("An error was encountered",ex);
+        }
+
+    }
+
+    @Override
+    public User login(Connection connection, String email, String password) {
+        try{
+            String query = "SELECT * FROM users WHERE email = :email AND password = :password";
+            return connection.createQuery(query)
+                    .addParameter("email",email)
+                    .addParameter("password",password)
+                    .throwOnMappingFailure(false)
+                    .executeAndFetchFirst(User.class);
+        } catch (Sql2oException ex){
+            throw new RuntimeException("An error was encountered", ex);
+        }
+
+    }
+
+    @Override
+    public boolean changePassword(Connection connection, String email, String password) {
+        try{
+            String query = "UPDATE users SET password = :password WHERE email = :email";
+            return connection.createQuery(query)
+                    .addParameter("password",password)
+                    .addParameter("email",email)
+                    .throwOnMappingFailure(false)
+                    .executeUpdate()
+                    .getResult()>0;
+        } catch (Sql2oException ex){
+            throw new RuntimeException("An error was encountered", ex);
+        }
+
+    }
+}
