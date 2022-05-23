5f5ee189eeaae72bfc045d55b579c612d49411ab
chacha <chachaerickjo@gmail.com>
2022-05-24 00:18:48 +0300

    test - add test for creating an account

---
+++ src/test/java/org/example/dao/UserDaoTest.java
@@ -0,0 +1,48 @@
+package org.example.dao;
+
+import org.example.database.DB;
+import org.example.database.DbImpl;
+import org.example.database.Seeder;
+import org.example.interfaces.IUser;
+import org.example.models.User;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserDaoTest {
+    private Connection con;
+    private DB db;
+
+
+    @BeforeEach
+    void setUp() {
+        db = new DbImpl();
+        con = db.connect();
+        Seeder.seed(con);
+    }
+
+    @AfterEach
+    void tearDown() {
+        Seeder.drop(con);
+    }
+
+    @Test
+    void createAccount() {
+        IUser userDao = new UserDao();
+        User user = new User("chacha","chacha@riko.com","understand","020-224-2525");
+        assertTrue(userDao.createAccount(con,user));
+
+    }
+
+    @Test
+    void login() {
+    }
+
+    @Test
+    void changePassword() {
+    }
+}
