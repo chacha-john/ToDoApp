f8dc11bce8d9379a5644dad7f2c5cff1fc9363ee
DruSadeMumba <drusademumba@gmail.com>
2022-05-23 22:00:58 +0300

    Create task interface methods implementing CRUD

--- src/main/java/org/example/models/Interface/InterfaceTask.java
+++ src/main/java/org/example/models/Interface/InterfaceTask.java
@@ -1,4 +1,23 @@
 package org.example.models.Interface;
 
+import org.example.models.Task;
+
+import java.util.List;
+
 public interface InterfaceTask {
+    //Implement CRUD
+
+    //CREATE
+    void add (Task task);
+
+    //READ
+    Task findById(int id);
+    List<Task> getAll(int userId);
+
+    //UPDATE
+    void update (int id, String name, String description, Boolean completed);
+
+    //DELETE
+    void deleteById(int id);
+    void clearAllTasks();
 }
