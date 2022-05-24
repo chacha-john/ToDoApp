d0dd5144fc78fd56bd3a533d397b09e3a73159ff
Terry <terrybmochire@gmail.com>
2022-05-24 22:21:34 +0300

    refactor delete function.

--- src/main/java/org/example/database/Seeder.java
+++ src/main/java/org/example/database/Seeder.java
@@ -8,18 +8,23 @@ public class Seeder {
             String users = "CREATE TABLE IF NOT EXISTS users( id SERIAL PRIMARY KEY, " +
                     "name VARCHAR, phone VARCHAR, email VARCHAR, password VARCHAR)";
 
+            String tasks = "CREATE TABLE IF NOT EXISTS tasks(id SERIAL PRIMARY KEY, userid INT, name VARCHAR, " +
+                    "description VARCHAR, createdon timestamp, completed BOOLEAN)";
+
             connection.createQuery(users).executeUpdate();
+            connection.createQuery(tasks).executeUpdate();
 
         } catch (Exception ex){
-            throw new RuntimeException("Unable to create table", ex);
+            throw new RuntimeException("Unable to create tables", ex);
         }
     }
 
     public static void drop(Connection connection){
         try{
             connection.createQuery("DROP TABLE users").executeUpdate();
+            connection.createQuery("DROP TABLE tasks").executeUpdate();
         } catch (Exception ex){
-            throw new RuntimeException("Unable to drop table", ex);
+            throw new RuntimeException("Unable to drop tables", ex);
         }
     }
 }
